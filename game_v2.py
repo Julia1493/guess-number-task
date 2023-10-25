60c73a958381d490107988d69e64ba383c592314
Julia1493 <julilipatova666@gmail.com>
2023-10-25 15:49:03 +0300

    Проект с угадыванием числа

---
+++ game_v2.py
@@ -0,0 +1,102 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+    return count
+
+def correction_predict(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+def binary_predict(number: int = 1) -> int:
+    """Бинарный поиск для нахождения значения. Берем левую и правую
+    границы и находим середину между ними. Если середина не равна числу
+    - определяем больше загаданное число или меньше. Далее обрезаем ненужную
+    половину. Функция принимает загаданное число и возвращает число попыток.
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    left = 0 # левая граница
+    right = 100 # правая граница
+
+
+    while left <= right:
+        count += 1
+        mid = (left + right) // 2 # нахождение середины
+        if mid == number:
+          break
+        elif mid < number: # увеличение левой границы
+          left = mid + 1
+        else: # или уменьшение правой
+          right = mid - 1 
+        
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
